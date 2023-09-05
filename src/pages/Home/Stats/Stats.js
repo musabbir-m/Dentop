3d061545b7b99f1a6c0b9f8d7ba671d5e8ca2348
musabbir-m <musabbir313@gmail.com>
2023-09-05 17:42:35 +0600

    added countup in stats

--- src/pages/Home/Stats/Stats.js
+++ src/pages/Home/Stats/Stats.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import CountUp, { useCountUp } from 'react-countup';
 import { BsCart4 } from "react-icons/bs";
 import { FaUserDoctor } from "react-icons/fa6";
 import { FaPeopleGroup } from "react-icons/fa6";
 
 const Stats = () => {
+
+    useCountUp({
+        ref: 'counter',
+        start:2,
+        end: 10,
+        enableScrollSpy: true,
+        scrollSpyDelay: 100,
+      });
+
+    useCountUp({
+        ref: 'counter2',
+        start:90,
+        end: 100,
+        enableScrollSpy: true,
+        scrollSpyDelay: 500,
+    })
+
+    useCountUp({
+        ref: 'counter3',
+        start:950,
+        end: 1000,
+        enableScrollSpy: true,
+        scrollSpyDelay: 1000,
+    })
+
+
   return (
     <div className='bg-gray-100 py-10 min-h-[500px]'>
      <div className=' py-10 px-4 mx-5 my-5 md:py-24 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-6  max-w-96 bg-white shadow-md'>
@@ -12,7 +39,8 @@ const Stats = () => {
                 <div className='w-24 h-24 flex justify-center items-center border-4 rounded-full border-blue-300'><BsCart4 className='w-16 h-16 text-blue-600'/></div>
             </div>
             <div className='w-80'>
-                <h2 className='text-6xl text-cyan-500'>10+</h2>
+                <h2 className='text-6xl text-cyan-500'> <span id="counter" /> +</h2>
+                
                 <p className='text-blue-800 text-2xl'>Years of Experience</p>
                 <p className='text-gray-500'> Incubate extensive scenarios without top-line quality vectors. Authoritatively engage </p>
             </div>
@@ -20,7 +48,10 @@ const Stats = () => {
         <div className='flex gap-4 '>
         <div className='w-24 h-24 flex justify-center items-center border-4 rounded-full border-blue-300'><FaUserDoctor className='w-16 h-16 text-blue-600'/></div>
             <div className='w-80'>
-                <h2 className='text-6xl text-cyan-500'>30+</h2>
+                
+                <h2 className='text-6xl text-cyan-500'>
+                <span id='counter2'/> +
+                </h2>
                 <p className='text-blue-800 text-2xl'>Experienced Doctors</p>
                 <p className='text-gray-500'> Incubate extensive scenarios without top-line quality vectors. Authoritatively engage </p>
             </div>
@@ -28,8 +59,8 @@ const Stats = () => {
         <div className='flex gap-4 '>
         <div className='w-24 h-24 flex justify-center items-center border-4 rounded-full border-blue-300'><FaPeopleGroup className='w-16 h-16 text-blue-600'/></div>
             <div className='w-80'>
-                <h2 className='text-6xl text-cyan-500'>300+</h2>
-                <p className='text-blue-800 text-2xl'>Happy Customers</p>
+                <h2 className='text-6xl text-cyan-500'><span id='counter3'/>+</h2>
+                <p className='text-blue-800 text-2xl'>Happy Clients</p>
                 <p className='text-gray-500'> Incubate extensive scenarios without top-line quality vectors. Authoritatively engage </p>
             </div>
         </div>
